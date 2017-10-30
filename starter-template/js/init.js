3f062507cf5ae18fdfd939d8a16f43c5c95a560b
Haniel <georgeconter@gmail.com>
2017-10-30 16:47:28 -0600

    Contactenos

--- starter-template/js/init.js
+++ starter-template/js/init.js
@@ -2,6 +2,8 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    $('.carousel').carousel({noWrap:true});
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
